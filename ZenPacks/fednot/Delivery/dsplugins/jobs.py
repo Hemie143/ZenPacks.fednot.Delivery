5bf85f566a13a2acc64fe85171be23b6a3083000
Laurent Hemeryck <hemie143@gmail.com>
2018-04-20 14:23:06 +0200

    Collection Jobs completed

--- ZenPacks/fednot/Delivery/dsplugins/jobs.py
+++ ZenPacks/fednot/Delivery/dsplugins/jobs.py
@@ -26,7 +26,7 @@ class Jobs(PythonDataSourcePlugin):
     )
 
     urls = {
-        'health': 'http://{}:{}/{}/management/metrics/job',
+        'job': 'http://{}:{}/{}/management/metrics/job',
     }
 
     @staticmethod
@@ -57,6 +57,7 @@ class Jobs(PythonDataSourcePlugin):
 
     def collect(self, config):
         log.debug('Starting Delivery health collect')
+        # TODO : cleanup job collect
 
         ip_address = config.manageIp
         if not ip_address:
@@ -67,10 +68,10 @@ class Jobs(PythonDataSourcePlugin):
         sem = DeferredSemaphore(1)
         for datasource in config.datasources:
             service_name = datasource.params['serviceName']
-            log.debug('collect datasource: {}'.format(datasource.datasource))
-            log.debug('collect component: {}'.format(datasource.component))
+            # log.debug('collect datasource: {}'.format(datasource.datasource))
+            # log.debug('collect component: {}'.format(datasource.component))
             url = self.urls[datasource.datasource].format(ip_address, datasource.zSpringBootPort, service_name)
-            log.debug('collect URL: {}'.format(url))
+            # log.debug('collect URL: {}'.format(url))
             # basic_auth = base64.encodestring('{}:{}'.format(datasource.zJolokiaUsername, datasource.zJolokiaPassword))
             # auth_header = "Basic " + basic_auth.strip()
             # TODO : move headers to Config properties
@@ -88,6 +89,11 @@ class Jobs(PythonDataSourcePlugin):
 
     def onSuccess(self, result, config):
         # log.debug('Success job - result is {}'.format(result))
+        # TODO : cleanup job onSuccess
+
+        status_maps = {'DONE': [0, 'Job {} is OK'],
+                       'ERROR': [5, 'Job {} is in error']
+                       }
 
         data = self.new_data()
 
@@ -98,30 +104,54 @@ class Jobs(PythonDataSourcePlugin):
                 metrics = json.loads(ddata[1])
                 ds_data[ds] = metrics
 
-        health_data = ds_data.get('health', '')
-        timestamp_now = int(time.time())
+        jobs_data = ds_data.get('job', '')
+        timestamp_now = int(time.time())    # UTC
+        # log.debug('timestamp_now: {}'.format(timestamp_now))
         for datasource in config.datasources:
             componentID = prepId(datasource.component)
             service_name = datasource.params['serviceName']
             r = re.match('{}_?(.*)'.format(service_name), componentID)
             component_label = r.group(1)
-            log.debug('comp: {}'.format(component_label))
-            job_list = [d for d in health_data if d['jobName'] == component_label]
+            # log.debug('comp: {}'.format(component_label))
+            job_list = [d for d in jobs_data if d['jobName'] == component_label]
             # log.debug('job_list: {}'.format(job_list))
             for job in job_list:
                 rundate = job['runDate']
+                # UTC
                 datetime_obj = datetime.datetime(rundate['year'], rundate['monthValue'], rundate['dayOfMonth'],
                                                  rundate['hour'], rundate['minute'], rundate['second'])
                 job['timestamp'] = int(datetime_obj.strftime('%s'))
+            # log.debug('job_list: {}'.format(job_list))
+            # TODO: Check that job_list isn't empty
             last_job = sorted(job_list, key=itemgetter('timestamp'), reverse=True)[0]
-            log.debug('last_job: {}'.format(last_job))
+            # log.debug('last_job: {}'.format(last_job))
             job_status = last_job['status']
-            job_age = timestamp_now - last_job['timestamp']
+            job_age = (timestamp_now - last_job['timestamp']) / 60
+            # log.debug('job_status: {}'.format(job_status))
+            # log.debug('timestamp_now: {}'.format(timestamp_now))
+            # log.debug('timestamp_job: {}'.format(last_job['timestamp']))
+            # log.debug('job_age      : {}'.format(job_age))
+            # log.debug('job_age2: {}'.format((timestamp_now - job_age) / 60))
+            job_status_map = status_maps.get(job_status, [3, 'Job {} has an unknown issue'])
+            data['values'][componentID]['status'] = job_status_map[0]
+            data['events'].append({
+                'device': config.id,
+                'component': componentID,
+                'severity': job_status_map[0],
+                'eventKey': 'JobHealth',
+                'eventClassKey': 'JobHealth',
+                'summary': job_status_map[1].format(componentID),
+                'message': job_status_map[1].format(componentID),
+                'eventClass': '/Status/App',
+                'zipName': last_job['zipName']
+            })
+            data['values'][componentID]['age'] = job_age
+            data['values'][componentID]['dataCount'] = last_job['dataCount']
+            data['values'][componentID]['missingCount'] = last_job['missingCount']
 
         # log.debug('Success job - result is {}'.format(len(ds_data)))
 
-        # log.debug('Success job - result is {}'.format(ds_data))
-
+        log.debug('Success job - data is {}'.format(data))
         return data
 
     def onError(self, result, config):
